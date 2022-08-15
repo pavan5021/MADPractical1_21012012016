36540b2f22cdb1b47a5b94e469fe24fe34208f5d
pavan5021 <pavanmodi21@gnu.ac.in>
2022-08-15 11:27:46 +0530

    Initial commit

---
+++ src/main/kotlin/practical1.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var i:Int=100
+    val f:Float=200.95F
+    val c:Char='a'
+    val s:String="Hello World"
+    val b:Boolean=true
+    val d:Double=45.56
+    val l:Long=12345
+    val sh:Short=-7
+    val by:Byte=127
+    println("Integer Value: $i")
+    println("Float Value: $f")
+    println("Character Value: $c")
+    println("String Value: $s")
+    println("Boolean Value: $b")
+    println("Double Value: $d")
+    println("Long Value: $l")
+    println("Short Value: $sh")
+    println("Byte Value: $by")
+}
