68a9dd92538ef9af99581e51c3276fe81493339a
pavan5021 <pavanmodi21@gnu.ac.in>
2022-08-15 11:32:55 +0530

    p2

---
+++ src/main/kotlin/practical2.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var a:Int = 10
+    var b = a.toDouble()
+    var g:String = "10"
+    var c = Integer.parseInt(g)
+    var d = g.toInt()
+    var e = g.toDouble()
+    println("Integer Value: $a")
+    println("Double Value (From Integer):$b")
+    println("String Value: $g")
+    println("Integer Value1 (From String):$c")
+    println("Integer Value2 (From String):$d")
+    println("Double Value (From String):$e")
+}
